a9dc73f2fd65a17d09dcbc6a7e8b6a8efa93d87a
Jeong kum ho <nashorn74@gmail.com>
2017-10-13 14:41:59 +0900

    샘플 API 템플릿 추가

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Project' });
+});
+
+router.post('/user/login', function(req, res) {
+	var id = req.body.id;
+	var password = req.body.password;
+	res.send(JSON.stringify({id:id,password:password}));
+});
+router.post('/user', function(req, res) {
+	var id = req.body.id;
+	var password = req.body.password;
+	res.send(JSON.stringify({id:id,password:password}));
+});
+router.get('/user', function(req, res) {
+	var rowid = req.query.rowid;
+	res.send(JSON.stringify({rowid:rowid}));
+});
+router.put('/user', function(req, res) {
+	var rowid = req.body.rowid;
+	var id = req.body.id;
+	var password = req.body.password;
+	res.send(JSON.stringify({rowid:rowid,id:id,
+		password:password}));
+});
+router.delete('/user', function(req, res) {
+	var rowid = req.body.rowid;
+	res.send(JSON.stringify({rowid:rowid}));
+});
+router.get('/user/list', function(req, res) {
+	res.send(JSON.stringify([]));
 });
 
 module.exports = router;
